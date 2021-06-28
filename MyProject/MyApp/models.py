b1ae44b0019ef47b18b5a9d8a4be2f4ca980a586
redbor <redbor24@gmail.com>
2021-06-28 09:04:47 +0300

    SF4.4 Формы поиска, добавления, редактирования и удаления постов

--- MyProject/MyApp/models.py
+++ MyProject/MyApp/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 
 # создаем модель сообщения
@@ -12,3 +13,9 @@ class Post(models.Model):
     def __str__(self):
         return f'"{self.header}", автор: {self.author.username.title()}'
 
+    # можно использовать этот метод для перехода на указанный в нём путь после успешного выполнения POST-метода,
+    # а можно просто прописать во views.py success_url = reverse_lazy('<нужный путь>') и не грузить модель
+    # фронтендовскими заморочками
+    # def get_absolute_url(self):  # Тут мы создали новый метод
+    #     # return reverse('post_details', args=[str(self.id)])
+    #     return reverse('post_list') #, args=[str(self.id)])
